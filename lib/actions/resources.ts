bcac2088e0dfea9b52ac82bd1a4e0e77a3d80f64
iskaa02 <eaemk@protonmail.com>
2025-02-07 17:22:15 +0200

    made createResource run in transaction

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -12,19 +12,20 @@ import { embeddings as embeddingsTable } from "../db/schema/embeddings";
 export const createResource = async (input: NewResourceParams) => {
   try {
     const { content } = insertResourceSchema.parse(input);
-
-    const [resource] = await db
-      .insert(resources)
-      .values({ content })
-      .returning();
-
     const embeddings = await generateEmbeddings(content);
-    await db.insert(embeddingsTable).values(
-      embeddings.map((embedding) => ({
-        resourceId: resource.id,
-        ...embedding,
-      })),
-    );
+    db.transaction(async (tx) => {
+      const [resource] = await tx
+        .insert(resources)
+        .values({ content })
+        .returning();
+
+      await tx.insert(embeddingsTable).values(
+        embeddings.map((embedding) => ({
+          resourceId: resource.id,
+          ...embedding,
+        })),
+      );
+    });
 
     return "Resource successfully created and embedded.";
   } catch (error) {
